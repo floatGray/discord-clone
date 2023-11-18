74e1c15c2a1106012a28f03bceacc1594dde8d49
floatGray <599316783@qq.com>
2023-11-18 11:38:59 +0800

    配置prisma和云服务，初始化登录逻辑

---
+++ lib/db.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db; //在开发环境中，让prisma不至于每次热更新都重新创建
