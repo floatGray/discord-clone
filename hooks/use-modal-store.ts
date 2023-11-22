629ad4ccded0a208ffc62ccd50771e4694ab8833
floatGray <599316783@qq.com>
2023-11-22 15:58:25 +0800

    完成创建新服务器的modal组件，添加账户和暗黑模式切换的组件

---
+++ hooks/use-modal-store.ts
@@ -0,0 +1,18 @@
+import { create } from 'zustand';
+
+export type ModalType = 'createServer';
+
+interface ModalStore {
+  type: ModalType | null;
+  isOpen: boolean;
+  onOpen: (type: ModalType) => void;
+  onClose: () => void;
+}
+
+export const useModal = create<ModalStore>((set) => ({
+  type: null,
+  data: {},
+  isOpen: false,
+  onOpen: (type, data = {}) => set({ isOpen: true }),
+  onClose: () => set({ type: null, isOpen: false }),
+}));
