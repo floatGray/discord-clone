bc50921e90baad4523bc1d85a9f9299ace2a597c
floatGray <599316783@qq.com>
2023-11-23 10:33:06 +0800

    完成邀请链接功能

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,11 +1,17 @@
+import { Server } from '@prisma/client';
 import { create } from 'zustand';
 
-export type ModalType = 'createServer';
+export type ModalType = 'createServer' | 'invite';
+
+interface ModalData {
+  server?: Server;
+}
 
 interface ModalStore {
   type: ModalType | null;
+  data: ModalData;
   isOpen: boolean;
-  onOpen: (type: ModalType) => void;
+  onOpen: (type: ModalType, data?: ModalData) => void;
   onClose: () => void;
 }
 
@@ -13,6 +19,6 @@ export const useModal = create<ModalStore>((set) => ({
   type: null,
   data: {},
   isOpen: false,
-  onOpen: (type, data = {}) => set({ isOpen: true }),
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
   onClose: () => set({ type: null, isOpen: false }),
 }));
